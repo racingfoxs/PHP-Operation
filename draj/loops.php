8332f05373c0d5b949988565ffe63ce5ab9dc4b5
DR_GURUNG <racingfoxs@gmail.com>
2023-03-15 10:13:28 +0530

    chapter 8th

---
+++ draj/loops.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Loops</title>
+	</head>
+	<body>
+        <?php 
+        $a= array(1,2,4,6,6);
+        foreach ($a as $item ) {
+            echo $item . " ";
+        }
+        ?>
+        <br/>
+        <?php 
+        foreach ($a as $position => $item ) {
+            echo $position . "=" . $item .",";
+        }
+        ?>
+	
+	</body>
+</html>
