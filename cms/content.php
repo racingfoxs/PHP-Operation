32eb0fb7d5c9f55897716b972056ec551c2de2b1
DR_GURUNG <racingfoxs@gmail.com>
2023-03-16 10:36:31 +0530

    completed cms

--- cms/content.php
+++ cms/content.php
@@ -1,25 +1,46 @@
 <?php require_once('./includes/functions.php'); ?>
 <?php require_once('./includes/dbconnect.php'); ?>
 <?php include('./includes/header.php'); ?>
+<?php
+if (isset($_GET['subj'])) {
+	$sel_subj = $_GET['subj'];
+	$sel_page = "";
+} elseif (isset($_GET['page'])) {
+	$sel_subj = "";
+	$sel_page = $_GET['page'];
+} else {
+	$sel_subj = "";
+	$sel_page = "";
+}
+
+// echo $sel_subj; checking we are getting it or not
+// echo $sel_page;
+?>
 
 <div>
     <div>
-        <aside class="w-full p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100">
+        <aside class="w-full h-screen p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100">
             <nav class="space-y-8 text-sm">
                 <div class="space-y-2">
                     <h2 class="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Nav</h2>
                     <div class="flex flex-col space-y-1">
-                        <?php
-                        $query = "SELECT * FROM subjects";
-                        $result = mysqli_query($connection, "SELECT * FROM subjects");
-                        while ($row = mysqli_fetch_array($result)) {
-                            echo "<a>{$row["menu_name"]}</a>";
-
-                            $results = mysqli_query($connection, "SELECT * FROM pages WHERE subject_id = {$row["id"]}");
-                            while ($row = mysqli_fetch_array($results)) {
-                                echo "<a classname=\"pb-12\"> {$row["menu_name"]}</a>";    
+                        <?php                        
+                        $subjects = get_all_subject();  
+                        while ($subject = mysqli_fetch_array($subjects)) {
+                            echo "<a";
+                            echo " href=\"content.php?subj=" . urlencode($subject['id']) . "\"";
+                            if($subject['id'] == $sel_subj){
+                                echo " class=\"font-bold\"";
+                            }                            
+                            echo ">{$subject["menu_name"]}</a;>";
+                            $pages = get_all_pages($subject["id"]);
+                            while ($page = mysqli_fetch_array($pages)) {
+                                echo "<a";
+                                if($page['id'] == $sel_page){
+                                    echo " class=\"font-bold\"";
+                                }    
+                                echo " href=\"content.php?page=" . urlencode($page["id"]) . "\" classname=\"pb-12 bg-teal-400\"> {$page["menu_name"]}</a>";    
                             }
-
                             echo "<br />";
                         } ?>
                         <!-- <a rel="noopener noreferrer" href="#">Plugins</a> -->
--- cms/includes/functions.php
+++ cms/includes/functions.php
@@ -1,1 +1,32 @@
-<?php ?>
+
+<?php
+
+function confirm_query($query) {
+    global $connection;
+    if ($connection->error) {
+        try {    
+            throw new Exception("MySQL error $connection->error <br> Query:<br> $query", $connection->errno);    
+        } catch(Exception $e ) {
+            echo "Error No: ".$e->getCode(). " - ". $e->getMessage() . "<br >";
+            echo nl2br($e->getTraceAsString());
+        }
+    }
+}
+
+function get_all_subject(){
+    global $connection;
+    $query = "SELECT * FROM subjects";
+    $subjects = $connection->query($query);
+    confirm_query($query); 
+    return $subjects;
+}
+
+function get_all_pages($s_id){
+    global $connection;
+    $query = "SELECT * FROM pages WHERE subject_id = $s_id";
+    $pages = $connection->query($query);
+    confirm_query($query); 
+    return $pages;
+}
+
+?>
